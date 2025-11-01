ad5e9c977cf6e39e9da04db426339cf9d81663b2
maxleiko <max.tricoire@gmail.com>
2025-11-01 16:37:51 +0100

    queries/indents.scm

--- queries/indents.scm
+++ queries/indents.scm
@@ -1,14 +1,3 @@
-; start indent after opening tokens
-["{" "[" "("] @indent.begin
-
-; end indent after closing tokens
-["}" "]" ")"] @indent.end
-
-; align multi-line constructs
-(fn_params) @indent.align
-(argument_list) @indent.align
-(type_params) @indent.align
-
-; dedent inside strings and comments
-(comment) @indent.dedent
-(string) @indent.dedent
+(_ "{" "}" @end) @indent
+(_ "[" "]" @end) @indent
+(_ "(" ")" @end) @indent
